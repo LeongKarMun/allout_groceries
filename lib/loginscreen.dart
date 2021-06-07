fa3a3a41383e2321a73712b9f3a7f1fb978cb157
LeongKarMun <karmun0823@gmail.com>
2021-06-07 22:57:18 +0800

    first commit

---
+++ lib/loginscreen.dart
@@ -0,0 +1,285 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'mainscreen.dart';
+import 'registrationscreen.dart';
+import 'package:http/http.dart' as http;
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _rememberMe = false;
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+  late SharedPreferences prefs;
+
+  @override
+  void initState(){
+    loadPref();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'All-out Groceries',
+      home: Scaffold(
+        body: Center(
+            child: SingleChildScrollView(
+                child: Column(
+          children: [
+            Container(
+                margin: EdgeInsets.fromLTRB(50, 50, 50, 10),
+                child: Image.asset('assets/images/allout1.png', scale: 0.5)),
+            SizedBox(height: 5),
+            Card(
+              margin: EdgeInsets.fromLTRB(30, 5, 30, 15),
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                child: Column(
+                  children: [
+                    Text('-Login-',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 23,
+                        )),
+                    TextField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: 'Email', icon: Icon(Icons.email)),
+                    ),
+                    TextField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                          labelText: 'Password', icon: Icon(Icons.lock)),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Checkbox(
+                            value: _rememberMe,
+                            onChanged:(bool? value){
+                              _onChange(value!);
+                            }
+                            ),
+                        Text("Remember Me")
+                      ],
+                    ),
+                    MaterialButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        minWidth: 100,
+                        height: 40,
+                        child: Text('Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                        onPressed: _onLogin,
+                        color: Colors.blue,
+                        ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              child:
+                  Text("Resigter New Account", style: TextStyle(fontSize: 16)),
+              onTap: _registerNewUser,
+            ),
+            SizedBox(height: 10),
+            GestureDetector(
+              child: Text("Forgot Password", style: TextStyle(fontSize: 16)),
+              onTap: _forgotPassword,
+            )
+          ],
+        ))),
+      ),
+    );
+  }
+
+  void _onChange(bool value) {
+    String _email = _emailController.text.toString();
+    String _password = _passwordController.text.toString();
+
+    if (_email.isEmpty || _password.isEmpty) {
+      Fluttertoast.showToast(
+          msg: "Email/Password is empty",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Color.fromRGBO(191, 30, 46, 50),
+          textColor: Colors.white,
+          fontSize: 16.0);
+      return;
+    }
+    setState(() {
+      _rememberMe = value;
+      storePref(value, _email, _password);
+    });
+  }
+
+  void _onLogin() {
+    String _email = _emailController.text.toString();
+    String _password = _passwordController.text.toString();
+
+    http.post(
+        Uri.parse(
+            "http://javathree99.com/s269926/alloutgroceries/php/login_user.php"),
+        body: {"email": _email, "password": _password}).then((response) {
+      print(response.body);
+      if (response.body == "success") {
+        Fluttertoast.showToast(
+            msg:
+                "Login success.",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Color.fromRGBO(191, 30, 46, 50),
+            textColor: Colors.white,
+            fontSize: 16.0);
+        Navigator.push(context, MaterialPageRoute(builder: (content) => MainScreen()));
+      } else {
+        Fluttertoast.showToast(
+            msg: "Login Failed",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Color.fromRGBO(191, 30, 46, 50),
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    });
+  }
+
+  void _registerNewUser() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (content) => RegistrationScreen()));
+  }
+
+  void _forgotPassword() {
+    TextEditingController _useremailController = new TextEditingController();
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Forgot Your Password ?", style: TextStyle(fontSize: 15)),
+            content: new Container(
+                height: 100,
+                child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text("Enter your recovery email: ", style: TextStyle(fontSize: 13)),
+                    TextField(
+                      controller: _useremailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: 'Email', icon: Icon(Icons.email)),
+                    )
+                  ],
+                ))),
+            actions: [
+              TextButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  print(_useremailController.text);
+                  _resetPassword(_useremailController.text.toString());
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  }),
+            ],
+          );
+        });
+  }
+
+  void _resetPassword(String emailreset) {
+    http.post(
+        Uri.parse(
+            "http://javathree99.com/s269926/alloutgroceries/php/reset_user.php"),
+        body: {"email": emailreset}).then((response) {
+      print(response.body);
+      if (response.body == "success") {
+        Fluttertoast.showToast(
+            msg:
+                "Please check your email.",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Color.fromRGBO(191, 30, 46, 50),
+            textColor: Colors.white,
+            fontSize: 16.0);
+      } else {
+        Fluttertoast.showToast(
+            msg: "Failed",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Color.fromRGBO(191, 30, 46, 50),
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    });
+  }
+
+  Future<void> storePref(bool value, String email, String password) async {
+    prefs = await SharedPreferences.getInstance();
+    if (value) {
+      await prefs.setString("email", email);
+      await prefs.setString("password", password);
+      await prefs.setBool("rememberme", value);
+      Fluttertoast.showToast(
+          msg: "Preferences stored",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Color.fromRGBO(191, 30, 46, 50),
+          textColor: Colors.white,
+          fontSize: 16.0);
+      return;
+    } else {
+      await prefs.setString("email", email);
+      await prefs.setString("password", password);
+      await prefs.setBool("rememberme", value);
+      Fluttertoast.showToast(
+          msg: "Preferences removed",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Color.fromRGBO(191, 30, 46, 50),
+          textColor: Colors.white,
+          fontSize: 16.0);
+      setState(() {
+        _emailController.text = "";
+        _passwordController.text = "";
+        _rememberMe = false;
+      });
+      return;
+    }
+  }
+
+  Future<void> loadPref() async {
+    prefs = await SharedPreferences.getInstance();
+    String _email = prefs.getString("email") ?? '';
+    String _password = prefs.getString("password") ?? '';
+    _rememberMe = prefs.getBool("remember") ?? false;
+
+    setState(() {
+      _emailController.text = _email;
+      _passwordController.text = _password;
+    });
+  }
+}
---
+++ lib/mainscreen.dart
@@ -0,0 +1,189 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  String _titlecenter = "Loading...";
+  List _productList = [];
+  late double screenHeight, screenWidth;
+  late SharedPreferences prefs;
+  String email = "";
+  int cartitem = 0;
+  late final String title;
+  TextEditingController _srcController = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProduct();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('Groceries Store'),
+      ),
+      body: Center(
+        child: Container(
+            child: Column(
+          children: [
+            TextFormField(
+              controller: _srcController,
+              decoration: InputDecoration(
+                hintText: "Search product",
+                suffixIcon: IconButton(
+                  onPressed: () => _searchProduct(_srcController.text),
+                  icon: Icon(Icons.search),
+                ),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.white24)),
+              ),
+            ),
+            if (_productList.isEmpty)
+              Flexible(child: Center(child: Text(_titlecenter)))
+            else
+              Flexible(
+                  child: Center(
+                      child: GridView.count(
+                          crossAxisCount: 2,
+                          childAspectRatio: (screenWidth / screenHeight) / 1,
+                          children: List.generate(_productList.length, (index) {
+                            return Padding(
+                                padding: const EdgeInsets.all(7),
+                                child: Card(
+                                  elevation: 10,
+                                  child: SingleChildScrollView(
+                                      child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        height: screenHeight / 5,
+                                        width: screenWidth / 1.1,
+                                        child: CachedNetworkImage(
+                                          imageUrl:
+                                              "https://javathree99.com/s269926/alloutgroceries/images/product/${_productList[index]['prid']}.jpg",
+                                        ),
+                                      ),
+                                      SizedBox(height: 2),
+                                      Padding(
+                                          padding:
+                                              EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                          child: Text(
+                                              "Product ID: " +
+                                                  _productList[index]['prid'],
+                                              style: TextStyle(fontSize: 16))),
+                                      SizedBox(height: 2),
+                                      Padding(
+                                          padding:
+                                              EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                          child: Text(
+                                              "Name: " +
+                                                  _productList[index]['prname'],
+                                              style: TextStyle(fontSize: 16))),
+                                      SizedBox(height: 2),
+                                      Padding(
+                                          padding:
+                                              EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                          child: Text(
+                                              "Price:RM " +
+                                                  _productList[index]
+                                                      ['prprice'],
+                                              style: TextStyle(fontSize: 16))),
+                                      SizedBox(height: 2),
+                                      Padding(
+                                          padding:
+                                              EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                          child: Text(
+                                              "Type: " +
+                                                  _productList[index]['prtype'],
+                                              style: TextStyle(fontSize: 16))),
+                                      SizedBox(height: 2),
+                                      Padding(
+                                          padding:
+                                              EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                          child: Text(
+                                              "Quantity: " +
+                                                  _productList[index]['prqty'],
+                                              style: TextStyle(fontSize: 16))),
+                                      SizedBox(height: 2),
+                                    ],
+                                  )),
+                                ));
+                          })))),
+          ],
+        )),
+      ),
+    );
+  }
+
+  /*Future<void> _loadPref() async {
+    prefs = await SharedPreferences.getInstance();
+    email = prefs.getString("email") ?? '';
+    print(email);
+    if (email == '') {
+      _loademaildialog();
+    } else {}
+  }*/
+
+  _loadProduct() {
+    http.post(
+        Uri.parse(
+            "https://javathree99.com/s269926/alloutgroceries/php/loadproduct.php"),
+        body: {}).then((response) {
+      if (response.body == "nodata") {
+        _titlecenter = "Sorry no product";
+        _productList = [];
+        return;
+      } else {
+        var jsondata = json.decode(response.body);
+        print(jsondata);
+        _productList = jsondata["products"];
+        _titlecenter = "";
+        setState(() {});
+      }
+    });
+  }
+
+    _searchProduct(String prname) {
+    http.post(
+        Uri.parse(
+            "https://javathree99.com/s269926/alloutgroceries/php/loadproduct.php"),
+        body: {"prname":prname}).then((response) {
+      if (response.body == "nodata") {
+        _titlecenter = "Sorry no product";
+        _productList = [];
+        return;
+      } else {
+        var jsondata = json.decode(response.body);
+        print(jsondata);
+        _productList = jsondata["products"];
+        _titlecenter = "";
+        setState(() {});
+      }
+    });
+  }
+
+  /*String titleSub(productList) {
+    if (title.length > 15) {
+      return title.substring(0, 15) + "...";
+    } else {
+      return title;
+    }
+  }*/
+
+  void _loademaildialog() {}
+}
